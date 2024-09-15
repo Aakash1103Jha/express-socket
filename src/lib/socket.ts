7a89cd60885dacb751db2d4c4731a09ad57bc49a
Aakash Jha <aakash.jha1103@gmail.com>
2024-09-16 00:30:36 +0530

    Simple console

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -6,9 +6,10 @@ export function startWs(server: any) {
   const io = new Server(server, { cors: { origin: '*' } });
   // *user connection
   io.on(Events.CONNECTION, (socket) => {
+    console.info('User connected');
     // *user disconnected
     socket.on(Events.DISCONNECT, () => {
-      console.log('user disconnected');
+      console.info('User disconnected');
     });
   });
 }
